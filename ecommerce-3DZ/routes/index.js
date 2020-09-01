d2a423023906679789dfe32e5e8d038904f27ebc
dialneus <dialneus@me.com>
2020-09-01 17:32:45 -0300

    LogIn 0.0

--- ecommerce-3DZ/routes/index.js
+++ ecommerce-3DZ/routes/index.js
@@ -6,5 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bienvenidos a 3DZ impresiones' });
 });
 
+/* GET LogIn page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Ingresá a tu cuenta de 3DZ impresiones' });
+});
+
+/* GET Register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Registrate en 3DZ impresiones' });
+});
 
 module.exports = router;
