6629d88a4f68397d224380b71be84bed781a5893
dialneus <dialneus@me.com>
2020-10-01 20:23:59 -0300

    Sprint 3.0

--- ecommerce-3DZ/routes/index.js
+++ ecommerce-3DZ/routes/index.js
@@ -8,22 +8,22 @@ router.get('/', function(req, res, next) {
 
 /* GET LogIn page. */
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Ingresá a tu cuenta de 3DZ impresiones' });
+  res.render('users/login', { title: 'Ingresá a tu cuenta de 3DZ impresiones' });
 });
 
 /* GET Register page. */
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Registrate en 3DZ impresiones' });
+  res.render('users/register', { title: 'Registrate en 3DZ impresiones' });
 });
 
-/* GET Register page. */
+/* GET Product page. */
 router.get('/productos', function(req, res, next) {
   res.render('productos', { title: 'Catalogo de productos' });
 });
 
 /* GET chart page. */
 router.get('/carrito', function(req, res, next) {
-  res.render('carrito', { title: 'Carrito de Compras 3DZ' });
+  res.render('products/carrito', { title: 'Carrito de Compras 3DZ' });
 });
 
 router.get('/formulario', function(req, res, next) {
