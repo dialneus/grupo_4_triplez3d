733316440db4e712810c0b17999113a57077fddf
dialneus <dialneus@me.com>
2020-11-25 15:23:40 -0300

    251120 AN

--- ecommerce-3DZ/routes/users.js
+++ ecommerce-3DZ/routes/users.js
@@ -61,9 +61,37 @@ router.post('/register', [
   })
 ], usersController.create);
 
+//Rutas a Cambio de Contraseña:
+router.get('/newPass', usersController.newPass);
+
+router.post('/newPass', [
+  check('email').isEmail().withMessage('El mail debe ser válido y debe estar completo.') ,
+  check('password').isLength({min:4}).withMessage('La constraseña debe tener al menos 4 carácteres.') ,
+  body("confirmPassword","password").custom(function (value, {req}){
+    if (req.body.password == value){
+      return true;
+    }else{ return false}
+  }).withMessage("Las contraseñas no coinciden"),
+//Validacion de mail:
+  body('email').custom(value => {
+    return db.Usuarios.findOne({
+      where: {
+        email: value
+      }
+    }).then((newUser) => {
+      if (!newUser) {
+        return Promise.reject('Usuario no registrado. Por favor registrese');
+    } else {
+      if (newUser.activo == 0) {
+        return Promise.reject('Usuario bloqueado. Comuniquese con el administrador');
+      }
+    }
+  })
+  }) ] , usersController.updateNewPass);
+
 //Ruta a Listado Usuarios:
 
-router.get('/listado',usersController.list);
+router.get('/listado',usersMiddleware.adminAccess, usersController.list);
 
 //Ruta a Detalle de Usuario:
 
@@ -87,11 +115,6 @@ router.post('/permission/:id', usersController.permission);
 
 router.post('/checkout/', usersController.checkout);
 
-
 //Ruta al carrito:
-
-//router.get('/chart', usersController.chart);
-
-
 
 module.exports = router;
