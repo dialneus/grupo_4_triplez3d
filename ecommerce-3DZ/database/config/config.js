fb90de9091ba93db8dd2198e5091f9fc95e8de90
marianjerez <mariotaebox08@gmail.com>
2020-11-28 00:10:01 -0300

    arreglando CSS footers, solapa contacto, y nosotros

--- ecommerce-3DZ/database/config/config.js
+++ ecommerce-3DZ/database/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password":"root",
+    "password":null,
     "database": "eCommerce-3DZ",
     "host": "localhost",
     "dialect": "mysql",
-    "port" : "8889"
+   /* "port" : "8889"*/
   },
   "test": {
     "username": "root",
